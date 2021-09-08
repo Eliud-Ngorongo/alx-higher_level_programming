5a880076e640a03a30802ebdacab710b6c45579c
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-08 12:24:29 +0300

    Lets fix pep

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-   """Print a string in uppercase"""
-   for c in str:
-     if c >= 'a' and c <= 'z':
-        c = chr(ord(c) - 32)
-     print("{:s}".format(c), end='')
-   print('')
+    """Print a string in uppercase"""
+    for c in str:
+        if c >= 'a' and c <= 'z':
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end='')
+    print('')
