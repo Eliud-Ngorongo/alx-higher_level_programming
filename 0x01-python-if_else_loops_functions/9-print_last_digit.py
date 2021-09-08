1345dc8a6c6eb9ee772b2068f84278cc0456c7cf
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-08 12:36:19 +0300

    Some modulus

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,14 @@
 def print_last_digit(number):
     if number < 0:
         last = number % -10
+        print(-last, end='')
         return(-last)
     else:
         last = number % 10
+        print(last, end='')
         return last
+
+print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
