1771dfe0b38bd013304105a125051bcf5b0742bb
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-09 19:09:50 +0300

    Lets equate the first n to 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = (len(sys.argv[1:]))
+    n = len(sys.argv[1:])
     if n == 0:
         print("{} arguments.".format(n))
-    else:
-        if n == 1:
+    elif n == 1:
             print("{} argument:".format(n))
             print("{}: {}".format(n, sys.argv[1]))
-        else:
+    else:
             print("{} arguments:".format(n))
             for i in range(n):
                 s = sys.argv[i+1]
