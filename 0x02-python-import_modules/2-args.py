f1819535d429fbab952a5ac2f55932670aeda84d
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-09 17:57:38 +0300

    Count the arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = (len(sys.argv[1:]))
+    if n <= 0:
+        print("{} arguments".format(n))
+    else:
+        if n == 1:
+            print("{} argument:".format(n))
+        else:
+            print("{} arguments:".format(n))
+            for i in range(1, n+1):
+                s = sys.argv[i]
+                print("{}: {}".format(i, s))
