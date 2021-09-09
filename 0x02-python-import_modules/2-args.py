30fc5579228b35896e43fcffe9db6c79fa57003d
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-09 19:05:41 +0300

    Cant find it

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
             print("{}: {}".format(n, sys.argv[1]))
         else:
             print("{} arguments:".format(n))
-            for i in range(1, n+1):
-                s = sys.argv[i]
-                print("{}: {}".format(i, s))
+            for i in range(n):
+                s = sys.argv[i+1] 
+                print("{}: {}".format(i+1, s))
