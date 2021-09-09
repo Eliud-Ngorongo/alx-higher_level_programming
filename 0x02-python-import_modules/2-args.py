fc9d43a6f0b721d2d86ea76d389ec21b84bfeb14
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-09 18:11:53 +0300

    change the arguements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     n = (len(sys.argv[1:]))
     if n <= 0:
-        print("{} argument.".format(n))
+        print("{} arguments.".format(n))
     else:
         if n == 1:
             print("{} argument:".format(n))
