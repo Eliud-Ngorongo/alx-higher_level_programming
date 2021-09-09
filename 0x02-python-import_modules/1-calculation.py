69b822d5a0969d5294e9e3fcf2f8d9cee600bc6f
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-09 15:45:19 +0300

    Import all modules once

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calc
+    from calc import add, sub, mul, div
     a = 10
     b = 5
 
-print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 
-print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
 
-print("{:.0f} / {:.0f} = {:.0f}".format(a, b, calc.div(a, b)))
+print("{:.0f} / {:.0f} = {:.0f}".format(a, b, div(a, b)))
