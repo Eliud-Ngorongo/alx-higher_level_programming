62646bdd59ac04529b14c4f8782d1fdbff7c61b4
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-10 08:40:57 +0300

    fixed check 8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if n == 0:
         print("{} arguments.".format(n))
     elif n == 1:
-            print("1 argument:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(n))
     for i in range(n):
-            s = sys.argv[i+1]
-            print("{}: {}".format(i+1, s))
+        s = sys.argv[i+1]
+        print("{}: {}".format(i+1, s))
