323260fdac13c520297064e59a0310a3507ea13f
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-13 15:51:58 +0300

    Second task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,8 +6,3 @@ def element_at(my_list, idx):
         return None
     else:
         return(my_list[idx])
-
-
-list = [1, 2, 6, 89]
-idx = 5
-print("Element at index {} is {}".format(idx, element_at(list, idx)))
