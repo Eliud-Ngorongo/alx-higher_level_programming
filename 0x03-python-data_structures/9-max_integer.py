1d0dab4c4b77cf0c3d0772f5c964123b1919f9c3
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-20 10:14:55 +0300

    Max number in the data structure

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    total = 0
+    max = my_list[0]
     if len(my_list) == 0:
         return None
-    for i in my_list:
-        if i > total:
-            total = i
-    return total
+    else:
+        for i in my_list:
+            if i > max:
+                max = i
+    return max
