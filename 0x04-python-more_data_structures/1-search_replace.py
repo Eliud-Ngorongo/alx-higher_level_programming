628431fc6dead9b0b07428517c8b2ccdaee96b36
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-21 11:11:03 +0300

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new = []
     for i in list:
-      if i == search:
-        new.append(replace)
+        if i == search:
+            new.append(replace)
     else:
         new.append(i)
     return new
