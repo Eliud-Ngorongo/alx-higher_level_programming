9e8b4c35ffc8db8da494fa67d6131ba4fd8b4667
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-28 15:41:57 +0300

    First task and READme

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """"Defining a class called square"""
+
+
 class Square:
-   """This a class square with no objects"""
-   pass
+    """This a class square with no objects"""
+    pass
