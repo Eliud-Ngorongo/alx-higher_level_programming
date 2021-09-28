4849834a260d2a995044cade48357a4d00e9dfea
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-28 15:27:53 +0300

    First task and READme

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""""Defining a class called square"""
+class Square:
+   """This a class square with no objects"""
+   pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is a repo on classes and objects in Python
