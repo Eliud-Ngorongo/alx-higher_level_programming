03fd528e47d6a3b036627b670adea24c30411bad
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-09-29 20:30:57 +0300

    __Private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module has a class square with a private instance size"""
+
+
+class Square:
+    """This a class with
+       the attribute class that is set to private"""
+
+    def __init__(self, size):
+        self.__size = size
