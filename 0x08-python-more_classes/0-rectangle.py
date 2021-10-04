c869a9a586cec98c28df630d758d1a84cf5e903d
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-10-04 17:47:11 +0300

    Readme plus first task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This is an empty class that defines a rectangle"""
+
+class Rectangle:
+    """It has no instances, attributes or methods"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# 0x08 python more classes
+ ## Intro
+This is a new repo with more projects on classes and decorators
