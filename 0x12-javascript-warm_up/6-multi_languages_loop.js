31fdef9a072643f65d83c6cdd9cba6ebe2c7177a
Eliud-Ngorongo <eliudnjenga@gmail.com>
2022-04-12 10:27:53 +0300

    Lets loop through

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const words = ['C is fun', 'Pyhton is cool', 'Javascript is amazing'];
+const words = ['C is fun', 'Pyhton is cool', 'JavaScript is amazing'];
 words.forEach(function (item) {
   console.log(item);
 });
